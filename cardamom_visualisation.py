6f07fac289c994cb0aef61eef6cb3b5954d2893a
Abhay B Prabhu <139116943+Abhay-Prabhu@users.noreply.github.com>
2025-05-01 16:57:05 +0530

    Update cardamom_visualisation.py

--- cardamom_visualisation.py
+++ cardamom_visualisation.py
@@ -147,7 +147,11 @@ def plot_auctioneer_qty_sold(data):
 # --- Main App ---
 
 def main():
-    st.title("Cardamom Auction Price Analysis")
+    st.set_page_config(
+    page_title="Cardamom Auction Price Analysis",
+    layout="wide",              # ← edge-to-edge
+    initial_sidebar_state="collapsed",
+)
     data = load_data()
 
     st.sidebar.title("Choose a chart")
